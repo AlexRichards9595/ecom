34afc7f5ec156c258673325697b8fbf717ef1df2
Alex <wcadidas15@gmail.com>
2018-03-19 15:14:57 -0400

    Changed it to a browse controller and stated building out controller tests for it

---
+++ src/test/java/org/wecancodeit/ecom/CatalogMvcTest.java
@@ -0,0 +1,30 @@
+package org.wecancodeit.ecom;
+
+import javax.annotation.Resource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.wecancodeit.ecom.catalog.BrowseController;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest 
+public class CatalogMvcTest {
+	
+	@Resource 
+	private MockMvc mvc;
+	
+	@Test
+	public void shouldRetreiveProducts () throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/products")).andExpect(status().isOk());
+	}
+	
+}
