0f2829ce05e37e70403617e2c06645a1af4699a1
Alex <wcadidas15@gmail.com>
2018-03-22 11:04:44 -0400

    assigned request methods to browse and maintenance controllers

--- src/main/java/org/wecancodeit/ecom/catalog/BrowseController.java
+++ src/main/java/org/wecancodeit/ecom/catalog/BrowseController.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class BrowseController {
 	@Resource
 	private CrudRepository<Product, Long> productRepo;
 	
-	@RequestMapping("/products")
+	@RequestMapping(path = "/products", method = RequestMethod.GET)
 	public Iterable<Product> findProducts() {
 		return productRepo.findAll();
 	}
